19a0b7072737518eebfd14d2f1772973da88f11e
Doniyor <donish1979@gmail.com>
2025-04-05 12:36:17 +0300

    finished readme and that's all, ready for submit hw 24

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,7 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.catalog, name="catalog"),
+    path("product_detail/<int:pk>", views.product_detail, name="product_detail"),
     path("contacts/", views.contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
 
 
-def home(request) -> HttpResponse:
+def catalog(request) -> HttpResponse:
     """
-    Функция возвращает страницу home.html.
+    Функция возвращает страницу catalog.html.
     """
-    return render(request, "catalog/home.html")
+    products = Product.objects.all()
+    context = {'products': products}
+
+    return render(request, "catalog/catalog.html", context=context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+
+    return render(request, "catalog/product_detail.html", context=context)
 
 
 def contacts(request) -> HttpResponse:
