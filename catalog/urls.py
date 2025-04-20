6b7a0053678bd856281b5b7ab64068bad8d4f944
Doniyor <donish1979@gmail.com>
2025-04-20 14:26:21 +0300

    finished hw 26.1 Forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import CatalogList, ProductDetail, Contacts, NewProduct, UpdateProduct, DeleteProduct
+from catalog.views import CatalogList, ProductDetail, Contacts, NewProduct, UpdateProduct, DeleteProduct, NewCategory, \
+    UpdateCategory, DeleteCategory
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", CatalogList.as_view(), name="catalog"),
+    path("new_category/", NewCategory.as_view(), name="new_category"),
+    path("update_category/<int:pk>", UpdateCategory.as_view(), name="update_category"),
+    path("delete_category/<int:pk>", DeleteCategory.as_view(), name="delete_category"),
+
     path("new_product/", NewProduct.as_view(), name="new_product"),
     path("product_detail/<int:pk>", ProductDetail.as_view(), name="product_detail"),
     path("update_product/<int:pk>", UpdateProduct.as_view(), name="update_product"),
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
-from .models import Product
+from .forms import CategoryForm, ProductForm
+from .models import Product, Category
 
 
 class CatalogList(ListView):
@@ -11,6 +12,38 @@ class CatalogList(ListView):
     context_object_name = 'products'
 
 
+class NewCategory(CreateView):
+    """ Класс добавления новой категории. """
+    model = Category
+    template_name = 'catalog/category_form.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('catalog:catalog')
+
+
+class UpdateCategory(UpdateView):
+    """ Класс редактирования категории. """
+    model = Category
+    template_name = 'catalog/category_form.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('catalog:catalog')
+
+
+class DeleteCategory(DeleteView):
+    """ Класс удаления категории. """
+    model = Category
+    template_name = 'catalog/category_confirm_delete.html'
+    success_url = reverse_lazy('catalog:catalog')
+
+
+class NewProduct(CreateView):
+    """ Класс добавления нового продукта. """
+    model = Product
+    template_name = 'catalog/product_form.html'
+    # fields = ['name', 'description', 'image', 'category', 'price']
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:catalog')
+    
+
 class ProductDetail(DetailView):
     """ Класс отображения подробной информации о продукте. """
     model = Product
@@ -18,19 +51,12 @@ class ProductDetail(DetailView):
     context_object_name = 'product'
 
 
-class NewProduct(CreateView):
-    """ Класс добавления нового продукта. """
-    model = Product
-    template_name = 'catalog/product_form.html'
-    fields = ['name', 'description', 'image', 'category', 'price']
-    success_url = reverse_lazy('catalog:catalog')
-
-
 class UpdateProduct(UpdateView):
     """ Класс изменения данных о продукте. """
     model = Product
     template_name = 'catalog/product_form.html'
-    fields = ['name', 'description', 'image', 'category', 'price']
+    # fields = ['name', 'description', 'image', 'category', 'price']
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:catalog')
 
 
